63d5662120de85739aa1c7ad4188fa5e1f85a2f4
Chaudhry Waqar Ali <waqar@we-over-i.com>
2022-05-05 11:52:23 +0500

    Added Fonts

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -8,13 +8,33 @@ const Header = (props) => {
 				<h1 className='text-8xl font-light text-white'>
 					Chaudhry <span className='font-bold'>Waqar</span> Ali
 				</h1>
-				<p>+92 324 4550066 - chwaqarali6@gmail.com</p>
+				<p>
+					<a className='hover:text-white' href='tel:+923244550066'>
+						+92 324 4550066
+					</a>
+					<span className='mx-4'>-</span>
+					<a className='hover:text-white' href='mailto:chwaqarali6@gmail.com'>
+						chwaqarali6@gmail.com
+					</a>
+				</p>
 				<p className='mb-9 text-4xl font-light opacity-60'>Main Boulevard Defence, Lahore, Punjab, Pakistan</p>
-				<p className='mb-9 flex h-20 w-[472px] items-center justify-center rounded-lg bg-white font-light text-black'>www.chwaqarali.com</p>
+				<a
+					className='flex h-20 w-[472px] items-center justify-center rounded-lg bg-white font-light text-black'
+					href='https://www.chwaqarali.com'
+					target='_blank'
+				>
+					www.chwaqarali.com
+				</a>
 				<div className='flex items-center'>
-					<img src={require('../../assets/icons/Instagram.png')} alt='Instagram' width='50' height='50' className='mr-24' />
-					<img src={require('../../assets/icons/LinkedIn.png')} alt='LinkedIn' width='50' height='50' className='mr-24' />
-					<img src={require('../../assets/icons/Github.png')} alt='Github' width='50' height='50' />
+					<a className='mx-3 cursor-pointer p-9' href='https://www.instagram.com/gattoographics/' target='_blank'>
+						<img src={require('../../assets/icons/Instagram.png')} alt='Instagram' width='50' height='50' />
+					</a>
+					<a className='mx-3 cursor-pointer p-9' href='https://www.linkedin.com/in/chwaqarali6/' target='_blank'>
+						<img src={require('../../assets/icons/LinkedIn.png')} alt='LinkedIn' width='50' height='50' />
+					</a>
+					<a className='mx-3 cursor-pointer p-9' href='https://github.com/chwaqarali6/' target='_blank'>
+						<img src={require('../../assets/icons/Github.png')} alt='Github' width='50' height='50' />
+					</a>
 				</div>
 			</div>
 		</div>
